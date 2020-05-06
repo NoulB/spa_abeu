be1492f67d6361cb5eeeff16880f29b2f07693a2
thiagotor44 <thiagotor44@gmail.com>
2020-05-06 16:16:01 -0300

    Corrigir a formatação de supervisor do contato que não estava funcionando; coloquei um limite em cada input igual ao que está no banco; na tela de index de paciente mudei a ultima consulta para idade e coloquei para pesquisar e calcular a idade do paciente atraves de um codigo com o Carbon ; criei um campo de idade na tela de criação e edição de paciente'idade' e coloquei para calcular a idade atraves de um codigo com o Carbon e um java script.
    dia:30 de abril 2020:
    Bloqueie o CPF nas telas de edição.
     Nesses Campo CPF. EU tirei a Formatação já que não precisa(ONBLUR)
    Adicionei um campo de data de Inscrição de paciente.
    Adicionei um variável createate no modelo de aluno e paciente.
    Coloquei o campo idade no view do aluno e na criação do aluno(mudei o id do rg para input3).
    Formatei todos os campos de todas a telas para ficarem no mesmo alinhados.
    Coloquei um redirecionamento quando registra um novo usuário de 10 segundos e um botão para entrar
    Coloquei as disponibilidades e turnos.
    
    06/05/2020
    Comentei a Disponibilidade e turno.
    Criei uma função de desformatar quando clicar, em, CEP, Telefone e Celular
    Mudei o cabeçalho do Layout de h1 para h3 e coloquei un col para centralizar.
    Alterei a tela Inícial do sistema

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -55,26 +55,23 @@
             </div>
         </div>
 
-                </div>
-            </div>
-        </footer>
     </div>
 
 
 
-   {{-- <div class="mr-sm-2">
 
-        <div>
-            <a href="/pacientes" class="btn btn-primary mb-2">Pacientes</a>
-        </div>
-        <div>
-            <a href="/supervisores" class="btn btn-primary mb-2">Supervisores</a>
-        </div>
-        <div>
-            <a href="/alunos" class="btn btn-primary mb-2">Alunos</a>
-        </div>--}}
+    {{-- <div class="mr-sm-2">
+
+         <div>
+             <a href="/pacientes" class="btn btn-primary mb-2">Pacientes</a>
+         </div>
+         <div>
+             <a href="/supervisores" class="btn btn-primary mb-2">Supervisores</a>
+         </div>
+         <div>
+             <a href="/alunos" class="btn btn-primary mb-2">Alunos</a>
+         </div>--}}
 
 
 
-    </div>
 @endsection
